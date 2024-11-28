9a9f5df0ce55570cbc650ea9105d7b1ed6aa2542
kingkw1 <king.kevin.w@gmail.com>
2024-11-28 11:36:26 -0500

    refactor main.py to streamline model selection and improve code clarity

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from matplotlib import pyplot as plt
 from core.generator import test_generator
-from core.process_data import process_training_data, process_test_data
-from core.train import test_network, initialize_network, set_seed, train_all_models, get_model, netvad
-from core.models import Net, NickNet, DenseNet
-from core.prepare_strong_files import prepare_strong_files
+from core.process_data import process_training_data
+from core.train import test_network, initialize_network, train_all_models, get_model, netvad
+from core.models import MODEL_STACK
 from core.prepare_files import prepare_files
-from evaluation.evaluate_webrtc_vad import webrtc_vad_accuracy
 
 
 def main():
@@ -21,18 +19,18 @@ def main():
 
     # Train all data
     train_all_models(data)
-    plt.show()
 
-    # Initialize the model & Load/Train it
-    # model = NickNet()
-    # model_name = 'gru'
-    # model = get_model(data, model, model_name)
+    # Select model for testing
+    model_name = 'net_large'
+    model_dict = MODEL_STACK[model_name]
+    model_type = model_dict['model']
+    model = get_model(data, model_type, model_name)
 
     # # Evaluate model with sample data
-    # netvad(model, data, title='Training Data')
-    # # netvad(model, strong_data, init_pos=400, title='STRONG Data')
+    netvad(model, data, title='Training Data')
+    # netvad(model, strong_data, init_pos=400, title='STRONG Data')
 
-    # plt.show()
+    plt.show()
 
 
 if __name__ == '__main__':
