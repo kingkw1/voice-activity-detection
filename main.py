22e9091a08d5aaf2f7c8867989accd5a6c94a7a2
kingkw1 <king.kevin.w@gmail.com>
2024-11-28 11:42:23 -0500

    evaluating default speech models with STRONG dataset

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from core.process_data import process_training_data
 from core.train import test_network, initialize_network, train_all_models, get_model, netvad
 from core.models import MODEL_STACK
 from core.prepare_files import prepare_files
-
+from core.prepare_strong_files import STRONGFileManager
 
 def main():
     # Set up data for use in neural networks
@@ -12,13 +12,13 @@ def main():
     data = process_training_data(speech_dataset, noise_dataset)
 
     # Define data generator 
-    test_generator(data)
+    # test_generator(data)
 
-    initialize_network()
-    test_network(data)
+    # initialize_network()
+    # test_network(data)
 
-    # Train all data
-    train_all_models(data)
+    # # Train all data
+    # train_all_models(data)
 
     # Select model for testing
     model_name = 'net_large'
@@ -26,9 +26,12 @@ def main():
     model_type = model_dict['model']
     model = get_model(data, model_type, model_name)
 
-    # # Evaluate model with sample data
+    # Evaluate model with sample data
     netvad(model, data, title='Training Data')
-    # netvad(model, strong_data, init_pos=400, title='STRONG Data')
+
+    # Evaluate model with STRONG data
+    strong_dataset = STRONGFileManager('processed_strong_data')
+    netvad(model, strong_dataset.data, init_pos=400, title='STRONG Data')
 
     plt.show()
 
